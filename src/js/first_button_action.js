987a3c7b8fe3e976474ce5a8471d124c9a8f2949
Diramix <danyagranny10@proton.me>
2026-01-27 18:08:13 +0300

    feat: add Next Music modal

--- src/js/first_button_action.js
+++ src/js/first_button_action.js
@@ -1,30 +1,38 @@
 function handleFirstButtonClick() {
+    // Hide Next Music modal
+    document.querySelector(".nextmusic_modal_wrapper").style.display = "none";
+
     // Show Toast
-    showToast("Background", "@Lucariochu1", "src/assets/toast_image.png", "https://www.youtube.com/watch?v=7uzgx5cl8gg");
+    showToast(
+        "Background",
+        "@Lucariochu1",
+        "src/assets/toast_image.png",
+        "https://www.youtube.com/watch?v=7uzgx5cl8gg",
+    );
 
     // Starting bg video
-    const bgVideo = document.getElementById('background-video');
+    const bgVideo = document.getElementById("background-video");
     if (bgVideo) bgVideo.play();
 
     // Profile Decoration
-    const profileDecoration = document.getElementById('apngStart');
-    profileDecoration.src = './src/assets/profile/intro.png';
+    const profileDecoration = document.getElementById("apngStart");
+    profileDecoration.src = "./src/assets/profile/intro.png";
 
     setTimeout(() => {
-        profileDecoration.src = './src/assets/profile/idle.png';
+        profileDecoration.src = "./src/assets/profile/idle.png";
     }, 6000);
 
     // Showing controls
-    const fb_container = document.getElementById('fb_container');
-    fb_container.style.opacity = '0';
-    fb_container.style.pointerEvents = 'none';
+    const fb_container = document.getElementById("fb_container");
+    fb_container.style.opacity = "0";
+    fb_container.style.pointerEvents = "none";
 
-    const mainWrapper = document.querySelector('.hidden_control');
-    mainWrapper.style.opacity = '1';
+    const mainWrapper = document.querySelector(".hidden_control");
+    mainWrapper.style.opacity = "1";
 }
 
 if (/Mobi|Android/i.test(navigator.userAgent)) {
-    window.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener("DOMContentLoaded", () => {
         const video = document.getElementById("background-video");
         if (video) video.remove();
     });
